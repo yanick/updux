d90d72148c2d4ba186a19650d961c64df5791c55
Yanick Champoux <yanick@babyl.dyndns.org>
2020-01-02 20:04:41 -0500

    feat!: middleware support refined

--- src/buildMiddleware/index.ts
+++ src/buildMiddleware/index.ts
@@ -1,54 +1,57 @@
-import fp from 'lodash/fp';
+import fp from "lodash/fp";
 
-import { Middleware, MiddlewareAPI, Dispatch } from 'redux';
-import { Dictionary, ActionCreator, Action, UpduxDispatch, UpduxMiddleware, UpduxMiddlewareAPI } from '../types';
+import { Middleware, MiddlewareAPI, Dispatch } from "redux";
+import {
+  Dictionary,
+  ActionCreator,
+  Action,
+  UpduxDispatch,
+  UpduxMiddleware,
+  UpduxMiddlewareAPI,
+  EffectEntry
+} from "../types";
 
-const MiddlewareFor = (type: any, mw: Middleware ): Middleware => api => next => action => {
-    if (type !== '*' && action.type !== type) return next(action);
+const MiddlewareFor = (
+  type: any,
+  mw: Middleware
+): Middleware => api => next => action => {
+  if (!["*", "^", "$"].includes(type) && action.type !== type)
+    return next(action);
 
-    return mw(api)(next)(action);
+  return mw(api)(next)(action);
 };
 
 type Next = (action: Action) => any;
 
-function sliceMw( slice: string, mw: Middleware ): Middleware {
-    return (api) => {
-        const getSliceState = () => fp.get(slice, api.getState() );
-        const getRootState = (api as any).getRootState || api.getState;
-        return mw({...api, getState: getSliceState, getRootState} as any )
-    };
+function sliceMw(slice: string, mw: Middleware): Middleware {
+  return api => {
+    const getSliceState =
+      slice.length > 0 ? () => fp.get(slice, api.getState()) : api.getState;
+    const getRootState = (api as any).getRootState || api.getState;
+    return mw({ ...api, getState: getSliceState, getRootState } as any);
+  };
 }
 
-function buildMiddleware<S=any>(
-    effects : Dictionary<UpduxMiddleware<S>>= {},
-    actions : Dictionary<ActionCreator>= {},
-    subduxes :any = {},
-): UpduxMiddleware<S>
- {
-
-    const subMiddlewares = fp.flow(
-        fp.mapValues( fp.get('middleware') ),
-        fp.toPairs,
-        fp.filter(x=>x[1]),
-        fp.map( ([ slice, mw ]: [ string, Middleware]) => sliceMw(slice,mw) )
-    )( subduxes );
+function buildMiddleware<S = any>(
+  middlewareEntries: any[] = [],
+  actions: Dictionary<ActionCreator> = {}
+): UpduxMiddleware<S> {
+  let mws = middlewareEntries
+    .map(([slice, actionType, mw, isGen]: any) =>
+      isGen ? [slice, actionType, mw()] : [slice, actionType, mw]
+    )
+    .map(([slice, actionType, mw]) =>
+      MiddlewareFor(actionType, sliceMw(slice, mw))
+    );
 
   return (api: UpduxMiddlewareAPI<S>) => {
-
     for (let type in actions) {
       const ac = actions[type];
-      api.dispatch[type] = (...args:any[]) => api.dispatch(ac(...args));
+      api.dispatch[type] = (...args: any[]) => api.dispatch(ac(...args));
     }
 
-    return (original_next: Next)=> {
-      return [
-        ...fp.toPairs(effects).map(([type, effect]) =>
-            MiddlewareFor(type,effect as Middleware)
-        ),
-        ...subMiddlewares
-      ]
-        .filter(x => x)
-        .reduceRight((next, mw) => mw(api)(next), original_next);
+    return (original_next: Next) => {
+      return mws.reduceRight((next, mw) => mw(api)(next), original_next);
     };
   };
 }
